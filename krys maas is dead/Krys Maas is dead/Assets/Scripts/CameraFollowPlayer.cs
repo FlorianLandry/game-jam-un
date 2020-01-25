ee18f5c6e40e6562ca7c77e0219976308558852b
Florian Landry <florian.landry71@gmail.com>
2020-01-25 14:23:45 -0500

    La nouvelle scene

---
+++ krys maas is dead/Krys Maas is dead/Assets/Scripts/CameraFollowPlayer.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class CameraFollowPlayer : MonoBehaviour
+{
+    public Transform joueur;
+    public Vector3 decalage;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = joueur.position + decalage;
+    }
+}
---
+++ krys maas is dead/Krys Maas is dead/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody joueur;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 2000f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            joueur.AddForce(speed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            joueur.AddForce(-speed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.Z))
+        {
+            joueur.AddForce(0, 0, speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            joueur.AddForce(0, 0, -speed * Time.deltaTime);
+        }
+    }
+}
